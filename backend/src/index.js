82988977812689247f30cc2f958fbc6e187ab401
Sodiaro <semiu.sodiq123@gmail.com>
2025-12-24 04:41:41 +0100

    fix: enable CORS for frontend requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,15 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    Credentials: true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
     connectDB();
-})
+});
 
