78c668cb4d905d28dbb14640fc6c6d631ab3f0a6
Sodiaro <semiu.sodiq123@gmail.com>
2025-12-24 19:33:49 +0100

    feat(ui): build settings page with theme picker and chat preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,15 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore} from "./store/useThemeStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
- 
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
+
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -29,7 +31,7 @@ const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
       </div>
     );
   return (
-    <div>
+    <div data-theme= {theme} >
       <Navbar />
 
       <Routes>
