bb4130c4391f1992e741fdad38a926f22e6d7bfb
Sodiaro <semiu.sodiq123@gmail.com>
2025-12-27 01:59:33 +0100

    fix(routing): use regex fallback route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { connectDB } from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
-import { app, server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
 import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "2mb" }));
@@ -19,10 +18,12 @@ app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 
 app.use(cookieParser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -30,13 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
-
